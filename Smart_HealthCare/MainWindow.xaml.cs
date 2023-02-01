65f93280cb48cc871389f00f92a8a5ce9d4ac74c
fineboy <fineboy0407@gmail.com>
2023-02-02 00:46:52 +0800

    MenuBar, Search Box, UserImage

--- Smart_HealthCare/MainWindow.xaml.cs
+++ Smart_HealthCare/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Smart_HealthCare
 {
@@ -24,5 +12,15 @@ namespace Smart_HealthCare
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+
     }
 }
